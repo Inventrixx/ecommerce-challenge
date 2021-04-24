92aab5ebd47f29e91f2c2ab7b532773768ed37ce
Inventrixx <inveniahadouken@gmail.com>
2021-04-24 15:33:38 -0300

    refactor: eslint

--- server.js
+++ server.js
@@ -1,44 +1,37 @@
-const express = require('express')
-const next = require('next')
-const bodyParser = require('body-parser')
-const routes = require('./api/routes')
+const express = require("express");
+const next = require("next");
+const bodyParser = require("body-parser");
+const routes = require("./api/routes");
 
-const dev = process.env.NODE_ENV !== 'production'
+const dev = process.env.NODE_ENV !== "production";
 const app = next({
-  dev
-})
-const handle = app.getRequestHandler()
+  dev,
+});
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express()
+  const PORT = process.env.PORT;
+  const server = express();
   server.use(
     bodyParser.json({
-      limit: '50mb'
+      limit: "50mb",
     })
-  )
+  );
   server.use(
     bodyParser.urlencoded({
-      limit: '50mb',
-      extended: true
+      limit: "50mb",
+      extended: true,
     })
-  )
+  );
 
-  routes(server)
+  routes(server);
 
-  // server.get('/a', (req, res) => {
-  //     return app.render(req, res, '/a', req.query)
-  // })
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
 
-  // server.get('/b', (req, res) => {
-  //     return app.render(req, res, '/b', req.query)
-  // })
-
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
-
-  server.listen(process.env.PORT, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${process.env.PORT}`)
-  })
-})
+  server.listen(PORT, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${PORT}`);
+  });
+});
