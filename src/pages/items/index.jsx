8c54562639e00fd34544ec2338dda2c0436feafb
Inventrixx <inveniahadouken@gmail.com>
2021-04-26 12:12:53 -0300

    feat: added products index component & page

---
+++ src/pages/items/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { searchItems } from "../../services/searchItemsService";
+import MyLayout from "../../components/MyLayout";
+import BreadCrumbs from "../../components/BreadCrumbs";
+import ProductsList from "../../components/ProductsList";
+
+export default function ProductsIndex() {
+  const router = useRouter();
+
+  const [searchResults, setSearchResults] = useState(null);
+
+  useEffect(() => {
+    const { search } = router.query;
+
+    if (search) {
+      const getItems = async () => {
+        try {
+          const responseSearch = await searchItems(search);
+
+          setSearchResults(responseSearch);
+        } catch (e) {
+          console.warn(e);
+        }
+      };
+
+      getItems();
+    }
+  }, [router.query.search]);
+
+  return (
+    <section className="products-index">
+      {searchResults && (
+        <>
+          <section className="products-breadcrumbs">
+            <BreadCrumbs categories={searchResults.categories} />
+          </section>
+          <ProductsList products={searchResults} />
+        </>
+      )}
+    </section>
+  );
+}
+
+ProductsIndex.Layout = MyLayout;
