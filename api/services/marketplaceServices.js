7897600c30d5a81225cb5c7b3d39b7c1803c391c
Inventrixx <inveniahadouken@gmail.com>
2021-04-28 10:32:46 -0300

    feat: improve errors handler for front & back

--- api/services/marketplaceServices.js
+++ api/services/marketplaceServices.js
@@ -1,16 +1,27 @@
 const axios = require("axios");
 
-
 exports.search = async (req, res) => {
   try {
-
     const itemsSearch = req.query.q;
     const resp = await axios.get(
       `${process.env.API_MERCADO_LIBRE}/sites/MLA/search?limit=${process.env.SEARCH_LIMIT}&q=${itemsSearch}`
     );
 
-    const respCategories = resp.data.filters.find(category => category.id === 'category');
-    const allCategories = respCategories.values[0].path_from_root.map(values => values.name);
+    const itemsFound = resp.data.results.length > 0;
+
+    if (!itemsFound) {
+      res.status(200).json({
+        categories: [],
+        items: [],
+      });
+    }
+
+    const respCategories = resp.data.filters.find(
+      (category) => category.id === "category"
+    );
+    const allCategories = respCategories.values[0].path_from_root.map(
+      (values) => values.name
+    );
 
     let decimals = Number.parseFloat(resp.data.price % 1).toFixed(2);
 
@@ -22,7 +33,7 @@ exports.search = async (req, res) => {
       price: {
         currency: item.currency_id,
         amount: Math.floor(item.price),
-        decimals: decimals
+        decimals: decimals,
       },
       picture: item.thumbnail,
       condition: item.condition,
@@ -39,8 +50,8 @@ exports.search = async (req, res) => {
     };
 
     res.status(resp.status).json(response);
+    return;
   } catch (e) {
-    console.log(e)
     res.status(500).json({
       status: 500,
       message: "Fallo en el servicio de Mercado Libre",
@@ -49,11 +60,11 @@ exports.search = async (req, res) => {
 };
 
 exports.searchId = async (req, res) => {
-  const {
-    id
-  } = req.params
+  const { id } = req.params;
   try {
-    const resp = await axios.get(`${process.env.API_MERCADO_LIBRE}/items/${id}`);
+    const resp = await axios.get(
+      `${process.env.API_MERCADO_LIBRE}/items/${id}`
+    );
     const description = await axios.get(
       `${process.env.API_MERCADO_LIBRE}/items/${id}/description`
     );
@@ -61,7 +72,6 @@ exports.searchId = async (req, res) => {
     let decimals = Number.parseFloat(resp.data.price % 1).toFixed(2);
 
     decimals = parseInt(decimals.toString().substring(2));
-
 
     const responseUniqueItem = {
       author: {
@@ -74,14 +84,14 @@ exports.searchId = async (req, res) => {
         price: {
           currency: resp.data.currency_id,
           amount: resp.data.price,
-          decimals: decimals
+          decimals: decimals,
         },
         category: resp.data.category_id,
         picture: resp.data.pictures[0].url,
         condition: resp.data.condition,
         free_shipping: resp.data.shipping.free_shipping,
         sold_quantity: resp.data.sold_quantity,
-        description: description.data.plain_text
+        description: description.data.plain_text,
       },
     };
 
